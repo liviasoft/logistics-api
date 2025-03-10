dce3339f770ed1c6a2e4532054e45cb4383971c3
Bankole <techybanky@gmail.com>
2025-03-10 16:36:47 +0100

    wip: developer can register app

--- src/modules/organizations/organization.module.ts
+++ src/modules/organizations/organization.module.ts
@@ -2,9 +2,10 @@ import { forwardRef, Module } from '@nestjs/common';
 import { OrganizationService } from './organization.service';
 import { OrganizationController } from './organization.controller';
 import { AuthModule } from '../auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [forwardRef(() => AuthModule)],
+  imports: [forwardRef(() => AuthModule), ConfigModule],
   controllers: [OrganizationController],
   providers: [OrganizationService],
   exports: [OrganizationService],
